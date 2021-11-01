3a244b4b27daaa3bcbf1f90b8a5f81a30dc5fca2
Keonwoo Kim <me@kanu.kim>
2021-11-01 09:01:10 +0900

    ci(circleci): add circle ci and fix existing tests to work

--- packages/frontend/src/App.test.tsx
+++ packages/frontend/src/App.test.tsx
@@ -1,7 +1,29 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 
+import SocketMock from 'socket.io-mock';
+import { useSocket } from 'socket.io-react-hook';
+
 import App from './App';
+
+jest.mock('socket.io-react-hook');
+
+beforeEach(() => {
+  const mockUseSocket = useSocket as jest.MockedFunction<typeof useSocket>;
+  const { socketClient } = new SocketMock();
+  mockUseSocket.mockImplementationOnce((namespaceKey: string) => {
+    (socketClient as any).namespaceKey = namespaceKey;
+    return ({
+      socket: socketClient as any,
+      connected: socketClient.connected,
+      error: undefined,
+    });
+  });
+});
+
+afterEach(() => {
+  jest.restoreAllMocks();
+});
 
 test('renders learn react link', () => {
   render(<App />);
