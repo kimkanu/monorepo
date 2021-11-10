e11f62f5298fb6d664c06e7dfb192a27eb81b354
Keonwoo Kim <me@kanu.kim>
2021-11-11 00:49:30 +0900

    refactor(frontend/test.tsx): refactor duplicated codes

--- packages/frontend/src/pages/Test.tsx
+++ packages/frontend/src/pages/Test.tsx
@@ -5,28 +5,34 @@ import { Link } from 'react-router-dom';
 import TempButton from '../components/TempButton';
 import DropdownTest from '../components/test/DropdownTest';
 
+/** 여기에 테스트할 컴포넌트를 넣어주세요! `pathname: ['설명', 컴포넌트]` 형식 */
+const componentsToTest: Record<string, [string, React.ReactElement]> = {
+  dropdown: ['Dropdown and Dialog', <DropdownTest />],
+};
+
 interface Props {
   name: string;
 }
 
 const Test: React.FC<Props> = ({ name }) => {
-  switch (name) {
-    case '': {
-      return (
-        <div className="w-full h-full max-w-sm flex flex-col justify-center items-center mx-auto gap-4">
-          <Link className="block relative w-full" to="/tests/dropdown">
-            <TempButton width="full" label="Dropdown and Dialog" />
+  if (name === '') {
+    return (
+      <div className="w-full h-full max-w-sm flex flex-col justify-center items-center mx-auto gap-4">
+        {Object.entries(componentsToTest).map(([pathname, [description]]) => (
+          <Link key={pathname} className="block relative w-full" to={`/tests/${pathname}`}>
+            <TempButton width="full" label={description} />
           </Link>
-        </div>
-      );
-    }
-    case 'dropdown': {
-      return <DropdownTest />;
-    }
-    default: {
-      return null;
-    }
+        ))}
+      </div>
+    );
   }
+
+  const index = Object.keys(componentsToTest).findIndex((key) => key === name);
+  if (index >= 0) {
+    return componentsToTest[index][1];
+  }
+
+  return null;
 };
 
 export default Test;
