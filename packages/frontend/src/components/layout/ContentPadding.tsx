14f3d7ed890556f821abd800174b4eceebcf0439
Keonwoo Kim <keonwoo@kaist.ac.kr>
2021-11-20 07:26:10 +0900

    fix(frontend): fix a bug in contentpadding.tsx

--- packages/frontend/src/components/layout/ContentPadding.tsx
+++ packages/frontend/src/components/layout/ContentPadding.tsx
@@ -9,19 +9,17 @@ const ContentPadding: React.FC<Props> = ({ isFooterPresent, children }) => {
   const [width] = useWindowSize();
 
   return (
-    <div className="overflow-y-auto overflow-x-hidden flex justify-center w-full">
-      <div
-        className="py-8 w-full"
-        style={{
-          height: `calc(100 * var(--wh) - env(safe-area-inset-top, 0px) - env(safe-area-inset-bottom, 0px) - ${isFooterPresent ? 140 : 64}px)`,
-          maxWidth: 1184,
-          paddingLeft: `calc(env(safe-area-inset-left, 0px) + ${width >= 960 ? 32 : 24}px)`,
-          paddingRight: `calc(env(safe-area-inset-right, 0px) + ${width >= 960 ? 32 : 24}px)`,
-        }}
-      >
+    <div
+      className="py-8 overflow-y-auto overflow-x-hidden flex justify-center w-full"
+      style={{
+        height: `calc(100 * var(--wh) - env(safe-area-inset-top, 0px) - env(safe-area-inset-bottom, 0px) - ${isFooterPresent ? 140 : 64}px)`,
+        paddingLeft: `calc(env(safe-area-inset-left, 0px) + ${width >= 960 ? 32 : 24}px)`,
+        paddingRight: `calc(env(safe-area-inset-right, 0px) + ${width >= 960 ? 32 : 24}px)`,
+      }}
+    >
+      <div className="w-full h-fit" style={{ maxWidth: 1184 }}>
         {children}
       </div>
-
     </div>
   );
 };
