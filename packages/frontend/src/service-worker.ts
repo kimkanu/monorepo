46d64bf52f139a2a48027d43a022640da80c94d9
Keonwoo Kim <keonwoo@kaist.ac.kr>
2021-11-22 05:42:51 +0900

    service worker test 2

--- packages/frontend/src/service-worker.ts
+++ packages/frontend/src/service-worker.ts
@@ -43,6 +43,11 @@ registerRoute(
       return false;
     }
 
+    // If this is a URL that starts with /_, skip.
+    if (url.pathname.startsWith('/api')) {
+      return false;
+    }
+
     // If this looks like a URL for a resource, because it contains
     // a file extension, skip.
     if (url.pathname.match(fileExtensionRegexp)) {
@@ -81,7 +86,7 @@ self.addEventListener('message', (event) => {
 
 // Any other custom service worker logic can go here.
 self.addEventListener('fetch', (event) => {
-  if (new URL(event.request.url).pathname.startsWith('/api/')) {
+  if (new URL(event.request.url).pathname.startsWith('/api')) {
     return false;
   }
   return true;
