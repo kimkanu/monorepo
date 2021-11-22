4dbb8f1a9514efc39201c4b5436c4b489254d0f4
Keonwoo Kim <keonwoo@kaist.ac.kr>
2021-11-22 11:36:39 +0900

    feat(lib): extract common types and interfaces

--- packages/lib/src/types/rest/classroom.ts
+++ packages/lib/src/types/rest/classroom.ts
@@ -1,5 +1,21 @@
+export type YouTubeVideo = YouTubeVideoSingle | YouTubeVideoPlaylist;
+
+export interface YouTubeVideoSingle {
+  type: 'single';
+  id: string;
+}
+
+export interface YouTubeVideoPlaylist {
+  type: 'playlist';
+  id: string;
+  index: number;
+}
+
 export interface ClassroomJSON {
   hash: string;
+  name: string;
   instructorId: string;
   memberIds: string[];
+  video: YouTubeVideo | null;
+  isLive: boolean;
 }
