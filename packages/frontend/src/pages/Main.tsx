91d40da3ade58190f05c3910077ba1ad2c95666c
Keonwoo Kim <keonwoo@kaist.ac.kr>
2021-11-22 12:05:13 +0900

    refactor(frontend): refactor slightly

--- packages/frontend/src/pages/Main.tsx
+++ packages/frontend/src/pages/Main.tsx
@@ -1,81 +1,62 @@
+import { ClassroomJSON } from '@team-10/lib';
 import React from 'react';
 import { useLocation, useHistory } from 'react-router-dom';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 
 import Dialog from '../components/alert/Dialog';
 import ClassList from '../components/classroom/ClassList';
 import JoinCreateContent from '../components/classroom/JoinCreateContent';
 import ContentPadding from '../components/layout/ContentPadding';
-import classroomState from '../recoil/classroom';
-import { Classroom } from '../types/classroom';
-
-function generateClassroomHash(): string {
-  const generateSyllable = (): string => {
-    const random = (arr: string[]) => arr[Math.floor(Math.random() * arr.length)];
-    const first = ['B', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'S', 'T'];
-    const second = ['A', 'E', 'I', 'O', 'U'];
-    const third = ['K', 'L', 'M', 'N', 'P', 'S', 'T', 'Z'];
-
-    return `${random(first)}${random(second)}${random(third)}`;
-  };
-
-  return `${generateSyllable()}-${generateSyllable()}-${generateSyllable()}`;
-}
-
-const classrooms: Classroom[] = [
-  {
-    name: '전산학특강<FE개발>',
-    isLive: true,
-    isMine: true,
-    hash: generateClassroomHash(),
-    videoId: null,
-  },
-  {
-    name: '컴퓨터 시스템',
-    isLive: true,
-    isMine: false,
-    hash: generateClassroomHash(),
-    videoId: null,
-  },
-  {
-    name: '알고리즘 개론',
-    isLive: false,
-    isMine: false,
-    hash: generateClassroomHash(),
-    videoId: null,
-  },
-  {
-    name: '전산기조직',
-    isLive: false,
-    isMine: true,
-    hash: generateClassroomHash(),
-    videoId: null,
-  },
-];
+import classroomsState from '../recoil/classrooms';
+import meState from '../recoil/me';
 
 const Main: React.FC = () => {
-  const classroom = useRecoilValue(classroomState.atom);
+  const classrooms = useRecoilValue(classroomsState.atom);
+  const me = useRecoilValue(meState.atom);
+  const addClassroom = useSetRecoilState(classroomsState.new);
   const location = useLocation();
   const history = useHistory();
 
   return (
     <>
-      <>
-        {/* Join or Create Class Dialog */}
-        <Dialog
-          visible={/^\/classrooms\/new\/?$/.test(location.pathname)}
-          onClose={history.length > 0 ? history.goBack : () => history.replace('/')}
-        >
-          <JoinCreateContent />
-        </Dialog>
+      {/* Join or Create Class Dialog */}
+      <Dialog
+        visible={/^\/classrooms\/new\/?$/.test(location.pathname)}
+        onClose={history.length > 0 ? history.goBack : () => history.replace('/')}
+      >
+        <JoinCreateContent
+          onCreate={async (name) => {
+            fetch('/api/classes', {
+              method: 'POST',
+              body: JSON.stringify({ name }),
+              headers: { 'Content-Type': 'application/json' },
+            })
+              .then((response) => response.json())
+              .then((response: { success: true; payload: ClassroomJSON }) => {
+                addClassroom(response.payload);
+              });
+          }}
+        />
+      </Dialog>
 
-        {/* Content */}
-        <ContentPadding isFooterPresent>
-          <h1 className="py-16 text-title text-center font-bold">My Classes</h1>
-          <ClassList classrooms={classrooms} onClickAdd={() => history.push('/classrooms/new')} />
-          <div className="transition-all duration-300" style={{ height: classroom?.videoId ? 152 : 64 }} />
-        </ContentPadding>
-      </>
+      {/* Content */}
+      <ContentPadding isFooterPresent>
+        {me.loading ? null : me.info ? (
+          <>
+            <h1 className="py-16 text-title text-center font-bold">My Classes</h1>
+            <ClassList
+              userId={me.info.stringId}
+              classrooms={classrooms}
+              onClickAdd={() => history.push('/classrooms/new')}
+            />
+            <div className="transition-all duration-300" style={{ height: classrooms[0]?.video ? 152 : 64 }} />
+          </>
+        ) : (
+          <>
+            <h1 className="py-16 text-title text-center font-bold">로그인 해주세요!</h1>
+          </>
+        )}
+      </ContentPadding>
     </>
   );
 };
