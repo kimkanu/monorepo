f5257ae5b89470cbb1b4daa6ccd693b1e68e1c0c
Sally-SH <gkstmdgml211@kaist.ac.kr>
2021-11-26 20:34:13 +0900

    refactor: remove useless change

--- bin/generateSSL.js
+++ bin/generateSSL.js
@@ -2,6 +2,6 @@ require('dotenv').config({ path: '.env.ssl' });
 
 const fs = require('fs');
 const execSync = require('child_process').execSync;
-//execSync(`rm -f proxy/certs/*`);
+execSync(`rm -f proxy/certs/*`);
 fs.mkdirSync('proxy/certs', { recursive: true });
 execSync(`mkcert -cert-file proxy/certs/ssl.crt -key-file proxy/certs/ssl.key "${process.env.NGINX_HOST}"`);
