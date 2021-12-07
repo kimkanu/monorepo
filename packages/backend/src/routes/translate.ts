10d74108bbfddc455137dcea7dc3b1c366a0725a
hongbeomyoo <yhb7261@kaist.ac.kr>
2021-12-08 01:26:43 +0900

    papago api implementing draft

--- packages/backend/src/routes/translate.ts
+++ packages/backend/src/routes/translate.ts
@@ -8,37 +8,52 @@ import Route from './route';
  * @returns Route
  */
 
-const express = require('express');
+/* 파파고 api 구현 부분 수정 */
+const request = require('request');
 
-const app = express();
+const apiurl = 'https://openapi.naver.com/v1/papago/n2mt';
 const clientid = 'YOUR_CLIENT_ID';
 const clientsecret = 'YOUR_CLIENT_SECRET';
 const query = '번역할 문장을 입력하세요.';
 
+const option = {
+  url: apiurl,
+  form: { source: 'ko', target: 'en', text: query },
+  headers: { 'X-Naver-Client-Id': clientid, 'X-Naver-Client-Secret': clientsecret },
+};
+
+function translation(error, response, body) {
+  if (!error && response.statusCode === 200) {
+    console.log(JSON.parse(body));
+  } else {
+    console.log(`error =  ${response.statusCode}`);
+  }
+}
+
+request.post(option, translation(error, Response, body));
+
+/*
 function login(req, res) {
-  const apiurl = 'https://openapi.naver.com/v1/papago/n2mt';
-  const request = require('request');
   const options = {
     url: apiurl,
     form: { source: 'ko', target: 'en', text: query },
-    headers: { 'X-Naver-Client-Id': clientid, 'X-Naver-Client-Secret': clientsecret }
+    headers: { 'X-Naver-Client-Id': clientid, 'X-Naver-Client-Secret': clientsecret },
   };
-  request.post(options, post)
+  request.post(options, post);
+}
 
 function post(error, response, body) {
-  if (!error && response.statusCode == 200) {
-    res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+  if (!error && response.statusCode === 200) {
+    res.writeHead(200, { 'Content-Type': 'text/json;charset=utf-8' });
     res.end(body);
-    } else {
-      res.status(response.statusCode).end();
-      console.log('error = ' + response.statusCode);
-    }
-  };  
-  
-  app.listen(3000, function () {
-    console.log('http://127.0.0.1:3000/translate app listening on port 3000!');
-  });
+  } else {
+    res.status(response.statusCode).end();
+    console.log('error = ' + response.statusCode);
+  }
+};
 
+app.listen(3000, console.log('http://127.0.0.1:3000/translate app listening on port 3000!'));
+*/
 export default function generateRoute(server: Server): Route {
   const route = new Route(server);
 
@@ -59,4 +74,4 @@ export default function generateRoute(server: Server): Route {
   );
 
   return route;
-};
+}
